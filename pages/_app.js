1c43398620ad47fa7ec2ef2d5a5e43b9524cfbd5
Wenchin <wenchin.ch@gmail.com>
2023-04-26 17:35:24 +0800

    Add openInDefaultBrowser to avoid google login browser issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import "@/styles/globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  function openInDefaultBrowser() {
+    if (window.opener === null) {
+      console.log("website opened in a default browser");
+      return;
+    }
+
+    console.log("website opened in a web-embedded browser");
+    const newWindow = window.open(window.location.href, "_blank");
+    newWindow.opener = null;
+  }
+
+  useEffect(() => {
+    openInDefaultBrowser();
+  }, []);
+
   return (
     <AuthProvider>
       <Component {...pageProps} />;
