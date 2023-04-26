e0deae99ddc2c0b77daa72cfb884746ed2798db7
Wenchin <wenchin.ch@gmail.com>
2023-04-26 17:57:54 +0800

    Fix alert for web-embedded browsers

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,11 @@ import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
   function openInDefaultBrowser() {
-    if (window.opener === null) {
-      console.log("website opened in a default browser");
-      return;
+    if (window.self !== window.top) {
+      alert(
+        "請以預設網路瀏覽器 (Chrome, Safari...) 開啟這個網頁，才能成功使用 Google 登入！"
+      );
     }
-    alert("請以預設網路瀏覽器 (Chrome, Safari...) 開啟這個網頁，才能成功使用 Google 登入！")
-    console.log("website opened in a web-embedded browser");
-    const newWindow = window.open(window.location.href, "_blank");
-    newWindow.opener = null;
   }
 
   useEffect(() => {
